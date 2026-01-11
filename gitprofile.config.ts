dc592c366130e5bfcf0060a461059d767eddb890
Louca Malerba <63046315+malerbe@users.noreply.github.com>
2026-01-11 18:01:23 +0100

    Add 'malerbe/Dual-sPLS' to manual projects list

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -25,7 +25,7 @@ const CONFIG = {
       },
       manual: {
         // Properties for manually specifying projects
-        projects: ['malerbe/Encoders_Explained', 'malerbe/Decoders_Explained', 'malerbe/QRT_Challenge'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
+        projects: ['malerbe/Encoders_Explained', 'malerbe/Decoders_Explained', 'malerbe/QRT_Challenge', 'malerbe/Dual-sPLS'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
       },
     },
     external: {
